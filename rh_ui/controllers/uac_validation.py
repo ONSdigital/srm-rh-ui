766535a90280b8c9a56efe5a23ad9a65a947b171
Kacper Prywata <kacper.prywata@ons.gov.uk>
2023-08-15 14:01:32 +0100

    UAC error handling

--- rh_ui/controllers/uac_validation.py
+++ rh_ui/controllers/uac_validation.py
@@ -1,2 +1,8 @@
+from flask import flash 
+
 def validate_uac(uac):
-    pass
+    if not uac:
+        flash('uac_empty')
+    if len(uac) != 16:
+        flash('uac_invalid_length')
+    # TODO handle invalid uac (no match in rh-service)
--- rh_ui/views/start.py
+++ rh_ui/views/start.py
@@ -1,9 +1,9 @@
 import logging
-from flask import Blueprint, render_template, request, flash, g, url_for, redirect, abort
+from flask import Blueprint, render_template, request, flash, g, url_for, redirect, flash
 from structlog import wrap_logger
-from rh_ui.controllers import uac_validation
+from rh_ui.controllers.uac_validation import validate_uac
 from rh_ui.views.lang_code_processing import setup_lang_code_processing
-from flask_babel import refresh, _
+from urllib.error import HTTPError
 
 logger = wrap_logger(logging.getLogger(__name__))
 
@@ -19,20 +19,20 @@ def start_get():
     logger.info(f"received {request.method} on endpoint '{request.endpoint}'",
                 method=request.method,
                 path=request.path)
-    error = request.data
-    # if error:
-    #    return render_template("start.html", lang_code=g.lang_code, field_messages_dict=error)
+       
+    #raise HTTPError('url', 401, "Message", 'headers', 'donno')
     return render_template("start.html", lang_code=g.lang_code)
 
 
-# cleaner way compared to putting it in the same function
 @start_bp.route("/start/", methods=["POST"])
 def start_post():
     flash("POST received, lang code: " + g.lang_code)
     uac = request.form.get('access-code').upper().replace(' ', '')
-    if not uac:
-        return redirect(url_for('start_bp.start_get', data='uac_empty'))
-    if len(uac) != 16:
-        return redirect(url_for('start_bp.start_get', data='uac_invalid_length'))
 
-    return redirect(url_for('start_bp.start_get', lang_code=g.lang_code, values=None))
+    validate_uac(uac)
+   
+    return redirect(url_for('start_bp.start_get', lang_code=g.lang_code))
+
+
+
+
