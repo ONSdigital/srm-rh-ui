fbd895ee18525d2046deac6cd4c08f7b9d6ff2f6
Ryan <ryan.grundy@ons.gov.uk>
2023-08-18 09:51:34 +0100

    Added Google Analytics

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ class BaseConfig:
     ACCOUNT_SERVICE_URL = os.getenv("ACCOUNT_SERVICE_URL")
     LANGUAGES = ['en', 'cy']
     EQ_URL = os.getenv('EQ_URL')
+    GTM_CONTAINER_ID = os.getenv('GTM_CONTAINER_ID')
+    GTM_TAG_ID = os.getenv('GTM_TAG_ID')
 
 
 class DevelopmentConfig(BaseConfig):
@@ -20,6 +22,8 @@ class DevelopmentConfig(BaseConfig):
     SECRET_KEY = os.getenv('SECRET_KEY', b'_5#y2L"F4Q8z\n\xec]/')
     RH_SVC_URL = os.getenv("RH_SVC_URL", "http://localhost:8071/")
     ACCOUNT_SERVICE_URL = os.getenv("ACCOUNT_SERVICE_URL", "http://localhost:9092/")
+    GTM_CONTAINER_ID = os.getenv('GTM_CONTAINER_ID', 'GTM_CONTAINER_ID_XXXXX')
+    GTM_TAG_ID = os.getenv('GTM_TAG_ID', 'GTM_TAG_ID_XXXXX')
 
 
 class TestingConfig(DevelopmentConfig):
