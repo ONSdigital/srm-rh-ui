42464f4c9d91da05d81d300a764e28c9c6cdb386
AdamHawtin <adam.hawtin@ons.gov.uk>
2023-08-31 12:22:19 +0100

    Add trailing slashes to all route definitions

--- rh_ui/views/info_pages.py
+++ rh_ui/views/info_pages.py
@@ -8,7 +8,7 @@ logger = wrap_logger(logging.getLogger(__name__))
 info_pages_bp = Blueprint("info_pages_bp", __name__, template_folder="../templates/info_pages")
 
 
-@info_pages_bp.route("/cookies", methods=["GET"])
+@info_pages_bp.route("/cookies/", methods=["GET"])
 def cookies() -> str:
     logger.info(f"received {request.method} on endpoint '{request.endpoint}'",
                 method=request.method,
@@ -16,7 +16,7 @@ def cookies() -> str:
     return render_template("cookies.html")
 
 
-@info_pages_bp.route("/privacy-and-data-protection", methods=["GET"])
+@info_pages_bp.route("/privacy-and-data-protection/", methods=["GET"])
 def privacy_and_data_protection() -> str:
     logger.info(f"received {request.method} on endpoint '{request.endpoint}'",
                 method=request.method,
