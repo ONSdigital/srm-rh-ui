3d72be6a8dcbf666c076185da0debd46570e980a
Ryan <ryan.grundy@ons.gov.uk>
2023-08-31 13:00:41 +0100

    Fix test

--- tests/unit/views/test_healthcheck.py
+++ tests/unit/views/test_healthcheck.py
@@ -5,6 +5,6 @@ def test_get_healthcheck_endpoint(test_client):
     expected_response = {
         'name': 'respondent-home-ui',
     }
-    response = test_client.get('/info')
+    response = test_client.get('/info/')
 
     assert expected_response == json.loads(response.text)
