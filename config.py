2a0695745f6a5cf7dcbb69e2342b54c467048cfe
Ryan <ryan.grundy@ons.gov.uk>
2023-09-01 14:15:02 +0100

    Update config

--- config.py
+++ config.py
@@ -6,7 +6,11 @@ class BaseConfig:
     HOST = os.getenv('HOST')
     LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", "INFO")
     RH_SVC_URL = os.getenv("RH_SVC_URL")
-    ACCOUNT_SERVICE_URL = os.getenv("ACCOUNT_SERVICE_URL")
+    DOMAIN_URL = os.getenv("DOMAIN_URL")
+    DOMAIN_URL_PROTOCOL = os.getenv("DOMAIN_URL_PROTOCOL")
+
+    # Account service url is a link back to our service that we send to eq as part of the token
+    ACCOUNT_SERVICE_URL = f"{DOMAIN_URL_PROTOCOL}{DOMAIN_URL}"
     LANGUAGES = ['en', 'cy']
     EQ_URL = os.getenv('EQ_URL')
     GTM_CONTAINER_ID = os.getenv('GTM_CONTAINER_ID')
@@ -14,14 +18,17 @@ class BaseConfig:
 
 
 class DevelopmentConfig(BaseConfig):
-    DEBUG = True
+    DEBUG = False
     PORT = os.getenv("PORT", 9092)
     HOST = os.getenv('HOST', '0.0.0.0')
     LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", "DEBUG")
     EQ_URL = os.getenv('EQ_URL', 'http://localhost:5000')
     SECRET_KEY = os.getenv('SECRET_KEY', b'_5#y2L"F4Q8z\n\xec]/')
+    DOMAIN_URL = os.getenv("DOMAIN_URL", "localhost:9092")
+    DOMAIN_URL_PROTOCOL = os.getenv("DOMAIN_URL_PROTOCOL", 'http://')
+
+    ACCOUNT_SERVICE_URL = f"{DOMAIN_URL_PROTOCOL}{DOMAIN_URL}"
     RH_SVC_URL = os.getenv("RH_SVC_URL", "http://localhost:8071/")
-    ACCOUNT_SERVICE_URL = os.getenv("ACCOUNT_SERVICE_URL", "http://localhost:9092/")
     GTM_CONTAINER_ID = os.getenv('GTM_CONTAINER_ID', 'GTM_CONTAINER_ID_XXXXX')
     GTM_TAG_ID = os.getenv('GTM_TAG_ID', 'GTM_TAG_ID_XXXXX')
 
