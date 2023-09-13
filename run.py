d02d07cbd1cf71cdd9e31f45e6322017ca82cc11
Adam <35268982+AdamHawtin@users.noreply.github.com>
2023-09-13 14:22:40 +0100

    1078 Set up gunicorn runner (#4)
    Co-authored-by: Ryan <ryan.grundy@ons.gov.uk>

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from rh_ui.app_setup import create_app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=app.config["DEBUG"], host="0.0.0.0", port=app.config["PORT"])
